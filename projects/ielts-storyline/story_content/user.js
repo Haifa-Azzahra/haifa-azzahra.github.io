0318b27b29b46a67de35dbef1849a849c2c51f61
Haifa-Azzahra <ahaifaaudrey@gmail.com>
2026-04-05 18:42:40 +0700

    update - ielts storyline

--- projects/ielts-storyline/story_content/user.js
+++ projects/ielts-storyline/story_content/user.js
@@ -2,10 +2,10 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6YoVTVkdIhB":
+      case "6QW71FFop9D":
         Script1();
         break;
-      case "5jTjwvA0M8U":
+      case "61KlsOcbTmO":
         Script2();
         break;
   }
@@ -15,105 +15,95 @@ function Script1()
 {
   var player = GetPlayer();
 
-var data = {
-  learner_name: player.GetVar("learner_name"),
-  current_band_score: player.GetVar("current_band_score"),
-  target_band_score: player.GetVar("target_band_score"),
-  session_count: player.GetVar("session_count"),
+var data = 
+  "learner_name=" + encodeURIComponent(player.GetVar("learner_name")) +
+  "&current_band_score=" + encodeURIComponent(player.GetVar("current_band_score")) +
+  "&target_band_score=" + encodeURIComponent(player.GetVar("target_band_score")) +
+  "&session_count=" + encodeURIComponent(player.GetVar("session_count")) +
 
-  task_type: player.GetVar("task_type"),
-  criterion_focus: player.GetVar("criterion_focus"),
-  smart_goal_action: player.GetVar("smart_goal_action"),
-  smart_goal_method: player.GetVar("smart_goal_method"),
-  smart_goal_weeks: player.GetVar("smart_goal_weeks"),
-  motivation_why: player.GetVar("motivation_why"),
-  self_efficacy: player.GetVar("self_efficacy"),
-  outcome_expect: player.GetVar("outcome_expect"),
-  anxiety_named: player.GetVar("anxiety_named"),
-  struggle_reframe: player.GetVar("struggle_reframe"),
+  "&task_type=" + encodeURIComponent(player.GetVar("task_type")) +
+  "&criterion_focus=" + encodeURIComponent(player.GetVar("criterion_focus")) +
+  "&smart_goal_action=" + encodeURIComponent(player.GetVar("smart_goal_action")) +
+  "&smart_goal_method=" + encodeURIComponent(player.GetVar("smart_goal_method")) +
+  "&smart_goal_weeks=" + encodeURIComponent(player.GetVar("smart_goal_weeks")) +
+  "&motivation_why=" + encodeURIComponent(player.GetVar("motivation_why")) +
+  "&self_efficacy=" + encodeURIComponent(player.GetVar("self_efficacy")) +
+  "&outcome_expect=" + encodeURIComponent(player.GetVar("outcome_expect")) +
+  "&anxiety_named=" + encodeURIComponent(player.GetVar("anxiety_named")) +
+  "&struggle_reframed=" + encodeURIComponent(player.GetVar("struggle_reframed")) +
 
-  gap_identified: player.GetVar("gap_identified"),
-  strategy_review: player.GetVar("strategy_review"),
-  strategy_refinement: player.GetVar("strategy_refinement"),
-  strategy_impact: player.GetVar("strategy_impact"),
-  strategy_realigned: player.GetVar("strategy_realigned"),
+  "&gap_identified=" + encodeURIComponent(player.GetVar("gap_identified")) +
+  "&strategy_review=" + encodeURIComponent(player.GetVar("strategy_review")) +
+  "&strategy_refinement=" + encodeURIComponent(player.GetVar("strategy_refinement")) +
+  "&strategy_realigned=" + encodeURIComponent(player.GetVar("strategy_realigned")) +
+  "&gap_impact=" + encodeURIComponent(player.GetVar("gap_impact")) + 
 
-  improvement_attribution: player.GetVar("improvement_attribution"),
-  difficulty_attribution: player.GetVar("difficulty_attribution"),
-  new_criterion_focus: player.GetVar("new_criterion_focus"),
-  new_intention: player.GetVar("new_intention"),
-  new_practice_action: player.GetVar("new_practice_action"),
-  new_practice_strategy: player.GetVar("new_practice_strategy"),
-  new_practice_condition: player.GetVar("new_practice_condition"),
-  new_practice_time: player.GetVar("new_practice_time")
-};
+  "&improvement_attribution=" + encodeURIComponent(player.GetVar("improvement_attribution")) +
+  "&difficulty_attribution=" + encodeURIComponent(player.GetVar("difficulty_attribution")) +
+  "&new_criterion_focus=" + encodeURIComponent(player.GetVar("new_criterion_focus")) +
+  "&new_intention=" + encodeURIComponent(player.GetVar("new_intention")) +
+  "&new_practice_action=" + encodeURIComponent(player.GetVar("new_practice_action")) +
+  "&new_practice_strategy=" + encodeURIComponent(player.GetVar("new_practice_strategy")) +
+  "&new_practice_condition=" + encodeURIComponent(player.GetVar("new_practice_condition")) +
+  "&new_practice_time=" + encodeURIComponent(player.GetVar("new_practice_time"));
 
 fetch("https://script.google.com/macros/s/AKfycbxDbpb2y8wn1ssebSyDdebCkK2NlFR7zNQC8hLY3A6lB6lpDb2_5AtfM62qYQWORgw2Qg/exec", {
   method: "POST",
   headers: {
-    "Content-Type": "text/plain;charset=utf-8"
+    "Content-Type": "application/x-www-form-urlencoded"
   },
-  body: JSON.stringify(data)
+  body: data
 })
-.then(response => response.text())
-.then(text => {
-  alert("SUCCESS: " + text);
-})
-.catch(error => {
-  alert("ERROR: " + error);
-});
+.then(res => res.text())
+.then(txt => alert("SUCCESS: " + txt))
+.catch(err => alert("ERROR: " + err));
 }
 
 function Script2()
 {
   var player = GetPlayer();
 
-var data = {
-  learner_name: player.GetVar("learner_name"),
-  current_band_score: player.GetVar("current_band_score"),
-  target_band_score: player.GetVar("target_band_score"),
-  session_count: player.GetVar("session_count"),
+var data = 
+  "learner_name=" + encodeURIComponent(player.GetVar("learner_name")) +
+  "&current_band_score=" + encodeURIComponent(player.GetVar("current_band_score")) +
+  "&target_band_score=" + encodeURIComponent(player.GetVar("target_band_score")) +
+  "&session_count=" + encodeURIComponent(player.GetVar("session_count")) +
 
-  task_type: player.GetVar("task_type"),
-  criterion_focus: player.GetVar("criterion_focus"),
-  smart_goal_action: player.GetVar("smart_goal_action"),
-  smart_goal_method: player.GetVar("smart_goal_method"),
-  smart_goal_weeks: player.GetVar("smart_goal_weeks"),
-  motivation_why: player.GetVar("motivation_why"),
-  self_efficacy: player.GetVar("self_efficacy"),
-  outcome_expect: player.GetVar("outcome_expect"),
-  anxiety_named: player.GetVar("anxiety_named"),
-  struggle_reframe: player.GetVar("struggle_reframe"),
+  "&task_type=" + encodeURIComponent(player.GetVar("task_type")) +
+  "&criterion_focus=" + encodeURIComponent(player.GetVar("criterion_focus")) +
+  "&smart_goal_action=" + encodeURIComponent(player.GetVar("smart_goal_action")) +
+  "&smart_goal_method=" + encodeURIComponent(player.GetVar("smart_goal_method")) +
+  "&smart_goal_weeks=" + encodeURIComponent(player.GetVar("smart_goal_weeks")) +
+  "&motivation_why=" + encodeURIComponent(player.GetVar("motivation_why")) +
+  "&self_efficacy=" + encodeURIComponent(player.GetVar("self_efficacy")) +
+  "&outcome_expect=" + encodeURIComponent(player.GetVar("outcome_expect")) +
+  "&anxiety_named=" + encodeURIComponent(player.GetVar("anxiety_named")) +
+  "&struggle_reframed=" + encodeURIComponent(player.GetVar("struggle_reframed")) +
 
-  gap_identified: player.GetVar("gap_identified"),
-  strategy_review: player.GetVar("strategy_review"),
-  strategy_refinement: player.GetVar("strategy_refinement"),
-  strategy_impact: player.GetVar("strategy_impact"),
-  strategy_realigned: player.GetVar("strategy_realigned"),
+  "&gap_identified=" + encodeURIComponent(player.GetVar("gap_identified")) +
+  "&strategy_review=" + encodeURIComponent(player.GetVar("strategy_review")) +
+  "&strategy_refinement=" + encodeURIComponent(player.GetVar("strategy_refinement")) +
+  "&strategy_realigned=" + encodeURIComponent(player.GetVar("strategy_realigned")) +
+  "&gap_impact=" + encodeURIComponent(player.GetVar("gap_impact")) + 
 
-  improvement_attribution: player.GetVar("improvement_attribution"),
-  difficulty_attribution: player.GetVar("difficulty_attribution"),
-  new_criterion_focus: player.GetVar("new_criterion_focus"),
-  new_intention: player.GetVar("new_intention"),
-  new_practice_action: player.GetVar("new_practice_action"),
-  new_practice_strategy: player.GetVar("new_practice_strategy"),
-  new_practice_condition: player.GetVar("new_practice_condition"),
-  new_practice_time: player.GetVar("new_practice_time")
-};
+  "&improvement_attribution=" + encodeURIComponent(player.GetVar("improvement_attribution")) +
+  "&difficulty_attribution=" + encodeURIComponent(player.GetVar("difficulty_attribution")) +
+  "&new_criterion_focus=" + encodeURIComponent(player.GetVar("new_criterion_focus")) +
+  "&new_intention=" + encodeURIComponent(player.GetVar("new_intention")) +
+  "&new_practice_action=" + encodeURIComponent(player.GetVar("new_practice_action")) +
+  "&new_practice_strategy=" + encodeURIComponent(player.GetVar("new_practice_strategy")) +
+  "&new_practice_condition=" + encodeURIComponent(player.GetVar("new_practice_condition")) +
+  "&new_practice_time=" + encodeURIComponent(player.GetVar("new_practice_time"));
 
 fetch("https://script.google.com/macros/s/AKfycbxDbpb2y8wn1ssebSyDdebCkK2NlFR7zNQC8hLY3A6lB6lpDb2_5AtfM62qYQWORgw2Qg/exec", {
   method: "POST",
   headers: {
-    "Content-Type": "text/plain;charset=utf-8"
+    "Content-Type": "application/x-www-form-urlencoded"
   },
-  body: JSON.stringify(data)
+  body: data
 })
-.then(response => response.text())
-.then(text => {
-  alert("SUCCESS: " + text);
-})
-.catch(error => {
-  alert("ERROR: " + error);
-});
+.then(res => res.text())
+.then(txt => alert("SUCCESS: " + txt))
+.catch(err => alert("ERROR: " + err));
 }
 
